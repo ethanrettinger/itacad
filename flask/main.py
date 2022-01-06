4ee994f4ef87a872e4537baa13ee5c5ef330bdc5
mpxf <diamond061@outlook.com>
2022-01-06 10:12:13 -0500

    Web server additions

--- flask/main.py
+++ flask/main.py
@@ -1,9 +1,15 @@
 # Import dependencies
 from flask import Flask, request, render_template, redirect
+import os
 
 # Initialize app object
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.abspath("../pages"))
+
+# '/'
+@app.route("/", methods=["GET"])
+def landing():
+    return render_template("landing/index.html")
 
 # Initialize web server
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
