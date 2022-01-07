ba90e23e435fa3a79ba3bacb5e22ec6e8c31b78c
exyzmc <79817320+exyzmc@users.noreply.github.com>
2022-01-07 08:26:02 -0500

    Added client-side functionality
    - Scrollable message list
    - Added example messages
    - Removed HTML from messages to prevent XSS
    - Removed form action and shit since it causes page reload
    - Imported jQuery from CDN
    - Formatted messages more properly
    - Added sendMessage JS function
    - Added smooth scroll whenever messages are sent and the page is loaded
    - Made the form align with the bottom of the DIV and keeping it there when scrolled (took longer than anything else to implement lol)
    - Tweaked some background colors for improved visiblity

---
+++ pages/msgboard/script.js
@@ -0,0 +1,74 @@
+// sendMessage function 
+
+function sendMessage(sender, content) {
+    console.log(content)
+    /* check if content is empty */
+    if(!content) { return; }
+    if (content.length == 0) { return; }
+    /* check if content is too long */
+    if (content.length > 500) { return; }
+    /* remove html from content */
+    content = content.replace(/<\/?[^>]+(>|$)/g, "");
+    /* remove newlines from content */
+    content = content.replace(/\n/g, "");
+    
+
+    /* create divs to hold message and grab examples from index.html */
+    let msgContainer = document.createElement("div");
+    let header = document.createElement("div");
+    header.className = "messageHeader";
+    let messageName = document.createElement("div");
+    messageName.className = "messageName";
+    if(sender == "self") {
+        msgContainer.classList = "message fromMe";
+        messageName.innerHTML = "<p>You</p>";
+    } else {
+        msgContainer.classList = "message fromOther";
+        messageName.innerHTML = "<p>" + sender + "</p>";
+    }
+    console.log(msgContainer)
+    let messageTime = document.createElement("div");
+    messageTime.className = "messageTime";
+    messageTime.innerHTML = "<p>" + new Date().toLocaleTimeString() + "</p>";
+
+    let messageBody = document.createElement("div");
+    messageBody.className = "messageBody";
+    messageBody.innerHTML = "<p>" + content + "</p>";
+ 
+    /* append elements to divs */
+    header.appendChild(messageName);
+    header.appendChild(messageTime);
+    msgContainer.appendChild(header);
+    msgContainer.appendChild(messageBody);
+
+    /* append msgContainer to message list */
+    let messageList = document.getElementById("messageList");
+    messageList.appendChild(msgContainer);
+}
+
+function scrollSmoothToBottom (id) {
+    var div = document.getElementById(id);
+    $('#' + id).animate({
+       scrollTop: div.scrollHeight - div.clientHeight
+    }, 400);
+ }
+
+scrollSmoothToBottom('messageList');
+// thank jesus christ and the rest of the world for stackoverflow :praying:
+
+// when an element is added to #messageList scroll to bottom
+$('#messageList').on('DOMNodeInserted', function() {
+    scrollSmoothToBottom('messageList');
+});
+
+$("form").submit(function(e) {
+    e.preventDefault();
+    // clear input
+    
+    // get input
+    let sender = "self";
+    let content = $("#messageInput").val();
+    $("input").val("");
+    // send message
+    sendMessage(sender, content);
+});
