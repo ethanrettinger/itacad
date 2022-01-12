b18d122537ef02280d2d5116a1265127259a649a
exyzmc <79817320+exyzmc@users.noreply.github.com>
2022-01-12 13:17:01 -0500

    Created users and messages db
    Tons of things I can't be asked to mention.
    
    TODO:
     - Create login page and create user (passwords will be hashed, active directory in nodeJS is bad)

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io');
 const fs = require('fs');
-
+const bcrypt = require('bcrypt');
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
@@ -18,13 +17,12 @@ const io = require('socket.io')(server, {
 
 const JSONdb = require('simple-json-db');
 const db = new JSONdb('data/messages.json');
-
+const userdb = new JSONdb('data/users.json');
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, '/pages/msgboard/index.html'));
 });
 app.get('/api', (req, res) => {
     // send data/messages.json to client
-    console.log(db.JSON())
     res.send(db.JSON());
 });
 
@@ -43,7 +41,7 @@ app.post('/register', async (req, res) => {
     });
 });
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'pages/msgboard')));
 
 io.on('connection', (socket) => {
 // tell all users that a new user has joined
@@ -53,9 +51,18 @@ io.on('connection', (socket) => {
     // Listen for chatMessage
   socket.on('message', msg => {
     io.emit('message', msg);
-    console.log(msg)
     // get messages.json and add new message to object
+    // if db has more than 50 messages delete the first one
     db.set(msg.messageID, msg);
+    if(Object.keys(db.JSON()).length < 100) {
+        
+        return;
+    } else {
+        db.delete(db.JSON()[Object.keys(db.JSON())[0]]?.messageID);
+        console.log('deleted')
+    }
+    
+    
   });
 
   // Runs when client disconnects
@@ -63,6 +70,35 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('oldUser', data);
     });
 });
+
+app.get('/users', (req, res) => {
+    // send data/users.json to client
+    res.sendFile(path.join(__dirname, 'data/users.json'));
+});
+
+app.post('/users', async (req, res) => {
+    // register user with username and password
+    try {
+        const salt = await bcrypt.genSalt();
+        const hash = await bcrypt.hash(req.body.password, salt);
+        const user = {
+            name: req.body.name,
+            username: req.body.username,
+            password: hash,
+            secret: req.body.secret,
+        }
+        let users = JSON.parse(fs.readFileSync(path.join(__dirname, 'data/users.json'), 'utf8'));
+        users.push(user);
+        fs.writeFileSync(path.join(__dirname, 'data/users.json'), JSON.stringify(users));
+    
+    
+    
+        res.status(200).send({ success: true, message: 'User registered successfully' });
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+});
+
 const PORT = 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
